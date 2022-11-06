a304d744ff66f3e760568a01e823646b4650ead8
vinitrrane <vinitrrane@gmail.com>
2022-11-06 11:59:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Axios from 'axios'
+import excused from './img/excused.png'
+
 
 function App() {
+
+  const [excuse, setExcuse] = useState('');
+
+  const funClick = (excuse) => {
+
+    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}`).then((res) => {
+      setExcuse(res.data[0])
+    })
+
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='heading'><h1>Excuse Generator</h1>
+        <h4>Generate an excuse for below categories</h4>
+        <div className='btnGroup'>
+          <button onClick={() => funClick('party')}>Party</button>
+          <button onClick={() => funClick('family')}>Family</button>
+          <button onClick={() => funClick('office')}>Office</button>
+          <button onClick={() => funClick('children')}>Children</button>
+          <button onClick={() => funClick('college')}>College</button>
+          <button onClick={() => funClick('funny')}>Funny</button>
+          <button onClick={() => funClick('unbelievable')}>Unbelievable</button>
+          <button onClick={() => funClick('developers')}>Developers</button>
+        </div></div>
+
+
+      <br /><br />
+
+
+      <div className='excuse'>
+        <h4>Category : {excuse?.category}</h4>
+        <br />
+        <h3>Excuse: <br /><br />{excuse?.excuse}</h3>
+        <img src={excused}></img>
+      </div>
     </div>
+
   );
 }
 
